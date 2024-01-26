df929a7546dc93324c3871b48afc591c29b802f5
nagvanshi9275 <106686974+nagvanshi9275@users.noreply.github.com>
2024-01-26 20:00:19 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# form2
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/nagvanshi9275/form2)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Main from "./Main";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+
+     <Main/>
+
+
+
+    </div>
+  );
+}
